46f6ea979854c603cd20dfcf5e93298e2f5544dd
musti-deputyp25 <musti@musti-deputyp25>
2025-11-30 19:29:40 +0100

    update

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"os/exec"
+	"time"
+)
+
+type Data struct {
+	Host        string
+	CurrentTime time.Time
+	Uptime      string
+}
 
 func getHome(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("home"))
+	w.Header().Add("Content-Type", "application/json")
+	host, _ := os.Hostname()
+	cmd := exec.Command("uptime")
+	uptime, _ := cmd.Output()
+	responseData := Data{
+		Host:        host,
+		CurrentTime: time.Now(),
+		Uptime:      string(uptime),
+	}
+	json.NewEncoder(w).Encode(responseData)
 }
